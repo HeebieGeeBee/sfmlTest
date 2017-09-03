a2b4ed3c925bacd2cfdbcf0a7c366205a5761872
HeebieGeeBee <geoffbull29@gmail.com>
2017-09-03 16:43:37 +0200

    added animation class along with caveman sprite

--- main.cpp
+++ main.cpp
@@ -1,19 +1,42 @@
 #include <SFML/Graphics.hpp>
+#include "Animation.h"
 
 int main()
 {
     // Create the main window
     sf::RenderWindow app(sf::VideoMode(800, 600), "SFML window");
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
+    sf::RectangleShape paddle;
+    paddle.setSize(sf::Vector2f(50, 20));
+    paddle.setFillColor(sf::Color(125,125,125));
+    paddle.setOutlineColor(sf::Color(255,0,0));
+    paddle.setOrigin(25.0f, 10.0f);
+    paddle.setPosition(app.getSize().x/2, app.getSize().y - 40);
+
+    sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+    player.setPosition(206.0f, 206.0f);
+
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("spritesheet_caveman.png");
+    player.setTexture(&playerTexture);
+
+    Animation animation(&playerTexture, sf::Vector2u(4,4), 0.3f);
+
+    float deltaTime = 0.0f;
+    sf::Clock clock;
+
+
+    sf::Vector2u textureSize = playerTexture.getSize();
+    textureSize.x /= 4;
+    textureSize.y /= 4;
+
+    player.setTextureRect(sf::IntRect(textureSize.x*3, 0, textureSize.x, textureSize.y));
+
 
 	// Start the game loop
     while (app.isOpen())
     {
+        deltaTime = clock.restart().asSeconds();
         // Process events
         sf::Event event;
         while (app.pollEvent(event))
@@ -23,12 +46,14 @@ int main()
                 app.close();
         }
 
+
+        animation.Update(0, deltaTime, false);
+        player.setTextureRect(animation.uvRect);
         // Clear screen
-        app.clear();
+        app.clear(sf::Color::White);
 
-        // Draw the sprite
-        app.draw(sprite);
-
+        app.draw(player);
+        app.draw(paddle);
         // Update the window
         app.display();
     }
